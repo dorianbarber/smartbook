453f96554f86ee15e3e3d9f42ea2a83e9f85c54d
Dorian Barber <dpb20@duke.edu>
2018-10-13 16:58:56 -0400

    added flask server

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 class App extends Component {
   render() {
@@ -11,14 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+
+          </div>
         </header>
       </div>
     );
