d9c69bcc72d4580ca4e2592c27d73ca9ced4b0d4
Dorian Barber <dpb20@duke.edu>
2018-10-13 17:45:49 -0400

    fixed node modules and got flask to connect with react

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import button from 'react-bootstrap';
+import axios from 'axios';
 import './style/App.css';
 
 class App extends Component {
@@ -13,13 +14,20 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <div>
-            <button onClick={request}>
+            <button onClick={this.request}>
               Hello
             </button>
           </div>
         </header>
       </div>
     );
+  }
+
+  request(){
+    axios.get('http://localhost:5000/')
+      .then(res => {
+        console.log(res.data)
+      });
   }
 }
 
