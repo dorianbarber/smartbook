cc554f131b968c5c751c519b956ae96eca8930a3
Sherry Hu <sherry.hu@duke.edu>
2018-10-13 21:29:59 -0400

    my commits

--- app/src/App.js
+++ app/src/App.js
@@ -3,21 +3,14 @@ import logo from './logo.svg';
 import button from 'react-bootstrap';
 import axios from 'axios';
 import './style/App.css';
+import Directory from "./directory.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <div>
-            <button onClick={this.request}>
-              Hello
-            </button>
-          </div>
+            <Directory />
         </header>
       </div>
     );
---
+++ app/src/directory.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Heading from "./heading.js"
+
+class Directory extends Component{
+
+	render(){
+		return(	
+			<div>
+			<h2>Course: Sociology</h2>
+			<ul>
+				< Heading title='Chapter 1'/>
+				< Heading title='Chapter 2'/>
+				< Heading title='Chapter 3'/>
+			</ul>
+			
+			<h2>Course: Psychology</h2>
+			<ul>
+				< Heading title='Chapter 1'/>
+				< Heading title='Chapter 2'/>
+				< Heading title='Chapter 3'/>
+			</ul>
+
+			<h2>Course: Economics</h2>
+			<ul>
+				< Heading title='Chapter 1'/>
+				< Heading title='Chapter 2'/>
+				< Heading title='Chapter 3'/>
+			</ul>
+
+			</div>
+		)
+	}
+}
+
+export default Directory;
